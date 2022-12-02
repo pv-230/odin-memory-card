2fa97322036f28ba6d2ccf53d04dc9385bf54d68
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-12-01 20:46:03 -0500

    Prevent FOUC by showing spinner until all resources loaded

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'normalize.css';
 import './app.css';
 import Header from '../header/header.jsx';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [appClassName, setAppClassName] = useState('app loading');
+
+  /**
+   * Hides the main content until all resources have loaded.
+   */
+  useEffect(() => {
+    function doneLoading() {
+      setLoading(false);
+      setAppClassName('app');
+    }
+
+    if (document.readyState === 'complete') {
+      doneLoading();
+    } else {
+      window.addEventListener('load', doneLoading);
+    }
+
+    return () => window.removeEventListener('load', doneLoading);
+  }, []);
+
   return (
-    <div className="app">
-      <Header />
-    </div>
+    <>
+      {loading && <div className="spinner">Loading</div>}
+      <div className={appClassName}>
+        <Header />
+      </div>
+    </>
   );
 }
 
