4c1614db6acf10f63e3cdb35532a33f42a339a1a
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-12-04 17:13:59 -0500

    Adjust scoreboard width

--- src/components/scoreboard/scoreboard.jsx
+++ src/components/scoreboard/scoreboard.jsx
@@ -3,17 +3,22 @@ import PropTypes from 'prop-types';
 import './scoreboard.css';
 
 function Scoreboard(props) {
-  const { currentScore, highScore } = props;
+  const { cardCount, currentScore, highScore } = props;
 
   return (
     <div className="scoreboard">
-      <div className="scoreboard__current-score">Current score: {currentScore}/16</div>
-      <div className="scoreboard__high-score">High score: {highScore}/16</div>
+      <div className="scoreboard__current-score">
+        Current score: {currentScore}/{cardCount}
+      </div>
+      <div className="scoreboard__high-score">
+        High score: {highScore}/{cardCount}
+      </div>
     </div>
   );
 }
 
 Scoreboard.propTypes = {
+  cardCount: PropTypes.number.isRequired,
   currentScore: PropTypes.number.isRequired,
   highScore: PropTypes.number.isRequired,
 };
